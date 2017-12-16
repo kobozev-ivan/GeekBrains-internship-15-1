b49da4f4c736ca2dbc1ca97856a0811c842a33c1
Anton Lapin <creativ_nah@mail.ru>
2017-12-16 16:33:04 +0300

    Создан пакет с классом загрузчика веб-страниц

---
+++ web-crawler/downloader/Downloader.java
@@ -0,0 +1,34 @@
+package downloader;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+
+public class Downloader {
+    private BufferedReader reader = null;
+    private String line = null;
+    private String addonLine;
+
+    public String download(String url) {
+        try {
+            URL site = new URL(url);
+            reader = new BufferedReader(new InputStreamReader(site.openStream()));
+            while ((addonLine = reader.readLine()) != null) {
+                line += addonLine + "\n";
+            }
+            reader.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        return line;
+    }
+
+}
