8d511799a9755b0d536c245d822b74e2dd278d11
Yuri Tveritin <kentyku@bk.ru>
2017-12-25 01:38:12 +0400

    Переписал метод поиска ссылок Sitemap'ов

--- web-crawler/src/parser/ParseRobotsDotTxt.java
+++ web-crawler/src/parser/ParseRobotsDotTxt.java
@@ -1,55 +1,80 @@
 package parser;
 
+import dbworker.SitesTableWorker;
 import downloader.Downloader;
 
-import java.io.*;
-import java.net.URL;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
-import java.util.zip.GZIPInputStream;
+public class ParseRobotsDotTxt {
+    //Инициирует запрос в БД через класс dbworker.SitesTableWorker
+    //получает список названий сайтов, у которых нет ссылок на HTML - страницы в БД
+    //инициирует поиск robots.txt данных сайтов 
+    //просматривает robots.txt в поисках ссылок на sitemap
+   
+    private  ArrayList<String> siteNames;//хранит имена сайтов
+    private  ArrayList<String> urlsSitemap;//хранит адреса Sitemap'ов
+    
+    /*
+    метод возвращает список ссылок SiteMap'ов неисследованных сайтов
+    */
+    public ArrayList<String> parseRobotsDotTxt() {
+        urlsSitemap= new ArrayList();
+        siteNames= new ArrayList();
+        siteNames = initDataBaseQuery();//запрос списка сайтов
 
-public class ParseRobotsDotTxt extends Thread{
-    //получает список robots.txt
-    //инициирует поиск robots.txt данных сайтов
-    //просматривает robots.txt в поисках ссылок на новые страницы
-
-    private TreeMap<String, Integer> pagesList = new TreeMap<>();
-    private TreeMap<String, Integer> unchRobotsList = new TreeMap<>();
-
-    public ParseRobotsDotTxt(TreeMap<String, Integer> unchRobotsList) {
-        this.unchRobotsList = unchRobotsList;
-    }
-
-    public void run(){
-        System.out.println("parseRobotsDotTxt begin");
-
-        Set<Map.Entry<String, Integer>> set = unchRobotsList.entrySet();
-        for (Map.Entry<String, Integer> item: set) {
-            Integer id = item.getValue();
-            String url = item.getKey();
-            pagesList.putAll(searchNewPageReferences(url, id));
+        for (String siteName: siteNames) {
+            urlsSitemap.add(searchSiteMapReference(siteName));//добавление ссылки SiteMap в список
+//            System.out.println(searchSiteMapReference(siteName));//вывод ссылки из SiteMap    
         }
-        System.out.println("parseRobotsDotTxt end");
+        return urlsSitemap;
     }
 
-    private TreeMap<String, Integer> searchNewPageReferences(String url, int id) {
-        TreeMap<String, Integer> newPages = new TreeMap<>();
-        String pageContent = new Downloader().download(url);
-        String[] splitContent = pageContent.split(" ");
+    private  ArrayList<String> initDataBaseQuery() {
+        ArrayList<String> list = new ArrayList<>();
+        SitesTableWorker sitesTableWorker = new SitesTableWorker();
+        sitesTableWorker.start();
+        try {
+            sitesTableWorker.join();//ждем завершения потока sitesTableWorker
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
 
-        for(int i = 0; i < splitContent.length; i++){
-            if(splitContent[i].equals("Sitemap:") || splitContent[i].equals("sitemap:")){
-                newPages.put(splitContent[i + 1], id);
+        try {
+            list = sitesTableWorker.getNoReferenceSiteNamesList();//возврат списка названий сайтов
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+
+        return list;
+    }
+
+    
+    
+    
+    private  String searchSiteMapReference(String siteName) {
+        String url = null;         
+        Downloader downloader = new Downloader();
+
+        String result = downloader.download(siteName + "/robots.txt");
+/*
+дробим результат на строки и ищем в каждой строке позицию символов Sitemap
+Считается, что в файле Robot.txt есть только единственная запись Sitemap
+*/
+        String[] lines = result.split("\n");//делим на строки и кладем в массив строк
+//ищем строку Sitemap: или sitemap: после которой идет его адрес
+        for(String line:lines){
+            int index=line.indexOf("Sitemap:");
+            if (index!=-1){
+               url=line.substring(index+8).replace( " ", "" );
             }
+            else{
+               index=line.indexOf("sitemap:");
+               if (index!=-1){
+               url=line.substring(index+8).replace( " ", "" );
+               }
+            }   
         }
-
-        return newPages;
-    }
-
-    public TreeMap<String, Integer> getPagesList() {
-        return pagesList;
+        return url;//возравщаем адрес sitemap
     }
 }
