ac67cea7b84cbf8abcf8c27d1c019bd88b8bf450
Yuri Tveritin <kentyku@bk.ru>
2018-01-08 08:12:11 +0400

    Добавлены абстрактные методы

--- web-crawler/src/parser/ParseHTML.java
+++ web-crawler/src/parser/ParseHTML.java
@@ -10,17 +10,25 @@ import java.util.TreeMap;
 public class ParseHTML extends Thread {
 
     private TreeMap<String, Integer> unchHTMLPagesList = new TreeMap<>();
-
+    //конструктор
     public ParseHTML(TreeMap<String, Integer> unchHTMLPagesList) {
         this.unchHTMLPagesList = unchHTMLPagesList;
     }
 
-    public ParseHTML(){
-
-    }
-
+    
     public void run() {
-
+        downloadHTML(); 
+        parseString();
     }
 
+    private void downloadHTML() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void parseString() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+        
 }
+
+
